64da2ac42b8c664f3eaef2c8759df04596e399fc
Edward He <ehe058@uottawa.ca>
2024-02-24 23:33:32 -0500

    added comments

--- Section 25 EJS/4.1 EJS Tags/index.js
+++ Section 25 EJS/4.1 EJS Tags/index.js
@@ -1,3 +1,13 @@
+/*
+EJS Tags
+<%= variable %>                   JavaScript Output
+<% JavaScript Code %>             JavaScript Execute
+<%- <h1>Hello</h1> %>             Render HTML
+<%% %%>                           for <% or %>
+<&# This is a comment >           Stop Execution           
+<%- include("FILE_NAME.ejs") %>   Insert another EJS file
+*/
+
 import express from "express";
 const app = express();
 const port = 3000;
